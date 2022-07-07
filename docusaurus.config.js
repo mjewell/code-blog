9664f17635a2710c906b5c2beee6ac505a2b2b68
Michael Jewell <michaeljewell9911@gmail.com>
2022-07-06 20:36:30 -0700

    mj - fix titles

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Michael Jewell's Blog",
+  tagline: "React, TypeScript, and more",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -52,7 +52,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Blog",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
